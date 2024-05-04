f93b24eedabba7300a0d953be6d70dbe44c0ee42
Vinicius Provenzano <vinicius@provenzano.me>
2024-05-04 16:08:58 +0200

    further clean up

--- pkgs/development/python-modules/metron_talker/default.nix
+++ pkgs/development/python-modules/metron_talker/default.nix
@@ -7,13 +7,7 @@
 , python3
 , pkgs
 }:
-#t pyEnv = python3.withPackages( ps: [
-#    ps.pydantic
-#    ps.pydantic-scim
-#    ps.pydantic-core
-#  ]
-#  );
-#
+
 buildPythonPackage rec {
   pname = "metron_talker";
   version = "0.1.5";
@@ -24,25 +18,11 @@ buildPythonPackage rec {
     sha256 = "sha256-/1UmhK0gFSwpb6fI0kKJXdMMPx8GeKGZ/LWhbiYzJMw=";
   };
   propagatedBuildInputs = [
- #   typing-extensions
- #   python3Packages.pytest
-    #python3.Packages.setuptools-scm
-  #  python3.pkgs.pydantic-core
- #  python3.pkgs.pydantic
- #  python3.pkgs.pydantic-scim
- #  python3.pkgs.invoke
- #  python3.pkgs.isort
- #  python3.pkgs.pytest
- #  python3.pkgs.tox
    python3.pkgs.mokkari
- #  
- #  pkgs.pyenv
-    #
   ];
   buildInputs = [
     typing-extensions
     python3Packages.pytest
-#    python3.pkgs.setuptools-scm
     python3.pkgs.pydantic-core
     python3.pkgs.pydantic
     python3.pkgs.pydantic-scim
@@ -67,9 +47,6 @@ buildPythonPackage rec {
     python3.pkgs.setuptools_scm
     pkgs.pyenv
   ];
-
-  #  pythonImportsCheck = [ "settngs" ];
-
   meta = with lib; {
     description = "Metron.cloud plugin for Comictagger";
     homepage = "https://github.com/lordwelch/metron_talker";
