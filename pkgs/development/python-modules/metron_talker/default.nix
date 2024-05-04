1a959596d9747eebb93d14667816a804cc5fe8ff
Vinicius Provenzano <vinicius@provenzano.me>
2024-05-04 16:08:30 +0200

    Clean up file

--- pkgs/development/python-modules/metron_talker/default.nix
+++ pkgs/development/python-modules/metron_talker/default.nix
@@ -5,15 +5,15 @@
 , python3Packages
 , setuptools-scm
 , python3
-,pkgs
+, pkgs
 }:
-let pyEnv = python3.withPackages( ps: [
-      ps.pydantic
-      ps.pydantic-scim
-      ps.pydantic-core
-    ]
-    );
-in
+#t pyEnv = python3.withPackages( ps: [
+#    ps.pydantic
+#    ps.pydantic-scim
+#    ps.pydantic-core
+#  ]
+#  );
+#
 buildPythonPackage rec {
   pname = "metron_talker";
   version = "0.1.5";
@@ -24,24 +24,25 @@ buildPythonPackage rec {
     sha256 = "sha256-/1UmhK0gFSwpb6fI0kKJXdMMPx8GeKGZ/LWhbiYzJMw=";
   };
   propagatedBuildInputs = [
-    typing-extensions
-    python3Packages.pytest
-    setuptools-scm
-    python3.pkgs.pydantic-core
-    python3.pkgs.pydantic
-    python3.pkgs.pydantic-scim
-    python3.pkgs.invoke
-    python3.pkgs.isort
-    python3.pkgs.pytest
-    python3.pkgs.tox
-    python3.pkgs.mokkari
-    python3.pkgs.setuptools_scm
-    pkgs.pyenv
+ #   typing-extensions
+ #   python3Packages.pytest
+    #python3.Packages.setuptools-scm
+  #  python3.pkgs.pydantic-core
+ #  python3.pkgs.pydantic
+ #  python3.pkgs.pydantic-scim
+ #  python3.pkgs.invoke
+ #  python3.pkgs.isort
+ #  python3.pkgs.pytest
+ #  python3.pkgs.tox
+   python3.pkgs.mokkari
+ #  
+ #  pkgs.pyenv
+    #
   ];
   buildInputs = [
     typing-extensions
     python3Packages.pytest
-    setuptools-scm
+#    python3.pkgs.setuptools-scm
     python3.pkgs.pydantic-core
     python3.pkgs.pydantic
     python3.pkgs.pydantic-scim
